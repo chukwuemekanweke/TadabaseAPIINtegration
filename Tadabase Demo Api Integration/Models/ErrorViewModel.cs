616c85e474d2b0c7751a49d976d409dafabb4f40
Chukwuemeka Nweke <emekanweke604@gmail.com>
2020-02-23 18:29:22 +0100

    Add project files.

---
+++ Tadabase Demo Api Integration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tadabase_Demo_Api_Integration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
